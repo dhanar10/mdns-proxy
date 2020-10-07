d108eeb02dcf30cd4bc7c3d054d9936b368a0113
Dhanar Adi Dewandaru <dhanar.adi.dewandaru@gmail.com>
2020-10-08 02:22:59 +0700

    Add root user check in install script and use temporary directory during installation

--- install.sh
+++ install.sh
@@ -3,15 +3,28 @@
 set -e
 set -o pipefail
 
-SITE_PACKAGES_PATH="$(python3 -c 'import site; print(site.getsitepackages()[0])')"
+if [ "$(id -u)" -ne "0" ]; then
+	sudo "$0"
+	exit "$?"
+fi
+
 SLIMDNS_PY_URL="https://raw.githubusercontent.com/dhanar10-forks/slimDNS-py3/076c98e53e8218e4c6491eafef9f9545dc8e78d5/slimDNS.py"
+SITE_PACKAGES_PATH="$(python3 -c 'import site; print(site.getsitepackages()[0])')"
+
+TEMP_DIR="$(mktemp -d)"
+
+trap "rm -rf $TEMP_DIR" EXIT
+
+pushd $TEMP_DIR
 
 wget -c "$SLIMDNS_PY_URL"
 cp slimDNS.py "$SITE_PACKAGES_PATH"
 
-cp mdns-proxy.py /usr/local/bin
-chmod +x /usr/local/bin/mdns-proxy.py
+install -m 755 mdns-proxy.py /usr/local/bin
 
 cp mdns-proxy.service /etc/systemd/system
 systemctl daemon-reload
 systemctl start mdns-proxy
+
+popd
+
