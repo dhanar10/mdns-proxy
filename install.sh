54eb887b6b9f334a3b67e64a1db2cf5289a880c6
Dhanar Adi Dewandaru <dhanar.adi.dewandaru@gmail.com>
2020-10-08 09:48:17 +0700

    Make install script standalone

--- install.sh
+++ install.sh
@@ -3,34 +3,35 @@
 set -e
 set -o pipefail
 
-SCRIPT_DIR="$(readlink -f "$0" | xargs dirname)"
-
 if [ "$(id -u)" -ne "0" ]; then
 	sudo "$0"
 	exit "$?"
 fi
 
+MDNS_PROXY_PY_URL="https://raw.githubusercontent.com/dhanar10/mdns-proxy/master/mdns-proxy.py"
+MDNS_PROXY_SERVICE_URL="https://raw.githubusercontent.com/dhanar10/mdns-proxy/master/mdns-proxy.service"
 SLIMDNS_PY_URL="https://raw.githubusercontent.com/dhanar10-forks/slimDNS-py3/076c98e53e8218e4c6491eafef9f9545dc8e78d5/slimDNS.py"
 
 TEMP_DIR="$(mktemp -d)"
 
 trap "rm -rf $TEMP_DIR" EXIT
 
-pushd $TEMP_DIR
+cd $TEMP_DIR
+
+wget -c "$MDNS_PROXY_PY_URL"
+wget -c "$MDNS_PROXY_SERVICE_URL"
+wget -c "$SLIMDNS_PY_URL"
 
 apt-get update
 apt-get install -y python3-pip
 pip3 install dnslib
 
-SITE_PACKAGES_PATH="$(python3 -c 'import site; print(site.getsitepackages()[0])')"
-wget -c "$SLIMDNS_PY_URL"
-cp slimDNS.py "$SITE_PACKAGES_PATH"
+SITE_PACKAGES_DIR="$(python3 -c 'import site; print(site.getsitepackages()[0])')"
+cp slimDNS.py "$SITE_PACKAGES_DIR"
+install -m 755 "mdns-proxy.py" /usr/local/bin
+cp "mdns-proxy.service" /etc/systemd/system
 
-install -m 755 "$SCRIPT_DIR/mdns-proxy.py" /usr/local/bin
-
-cp "$SCRIPT_DIR/mdns-proxy.service" /etc/systemd/system
 systemctl daemon-reload
 systemctl start mdns-proxy
 
-popd
-
+cd ..
